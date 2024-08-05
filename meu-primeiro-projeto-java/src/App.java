c040992dd6e9f4889c05002002d50aa367793b49
reinaldo-matheus <reinaldomatheus011@gmail.com>
2024-08-05 10:37:06 -0300

    alteração de mensagem a ser impressa.

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world my dev friend !");
+        System.out.println("Hello my dev friend !");
     }
 }
